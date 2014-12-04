af790b2d9ee11e20f1f4b54aff9efb525f667042
Hardik Varia <hardikvaria@gmail.com>
2014-12-04 15:30:07 +0000

    fix typos in path

--- setup.sh
+++ setup.sh
@@ -13,12 +13,12 @@ git config --global user.name "Hardik Varia"
 # zsh
 mkdir ~/.zprezto
 git clone --recursive https://github.com/sorin-ionescu/prezto.git ~/.zprezto/
-ln -sf ~/.zprezto/runcoms/zlogin ~/.zlogin
-ln -sf ~/.zprezto/runcoms/zlogout ~/.zlogout
+ln -sf ~/.zprezto/runcoms/zlogin    ~/.zlogin
+ln -sf ~/.zprezto/runcoms/zlogout   ~/.zlogout
 ln -sf ~/.zprezto/runcoms/zpreztorc ~/.zpreztorc
-ln -sf ~/.zprezto/runcoms/zprofile ~/.zprofile
-ln -sf ~/.zprezto/runcoms/zshenv ~/.zshenv
-ln -sf ~/.zprezto/runcom/zshrc ~/.zshrc
+ln -sf ~/.zprezto/runcoms/zprofile  ~/.zprofile
+ln -sf ~/.zprezto/runcoms/zshenv    ~/.zshenv
+ln -sf ~/.zprezto/runcoms/zshrc     ~/.zshrc
 
 echo "export MYVIMRC='.vim/vimrc'" >> ~/.zshrc
 echo "export EDITOR='vim --servername psql --remote-tab-wait'" >> ~/.zshrc
