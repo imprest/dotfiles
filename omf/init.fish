557e559e60695811be0dd7982c7a4939f549727a
Hardik Varia <hardikvaria@gmail.com>
2017-03-15 21:23:11 +0000

    Some adjustments

--- omf/init.fish
+++ omf/init.fish
@@ -5,6 +5,8 @@ set -xg LANG en_GB.UTF-8
 set -xg TERM 'xterm-256color'
 set -xg PGDATABASE 'mgp' # frequently used database
 
+set fish_greeting ""
+
 # Awesome balias
 balias pacU 'sudo yaourt -Syu'
 balias paci 'sudo yaourt -S'
