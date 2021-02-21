9a4903ca21a1c6cf9a85fd88acbb2fb604956ad8
Hardik Varia <hardikvaria@gmail.com>
2021-02-21 23:11:29 +0000

    Almost full port from init.vim to init.lua

--- init.lua
+++ init.lua
@@ -11,29 +11,6 @@ local function map(mode, lhs, rhs, opts)
   api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
-local fn = vim.fn
-local fmt = string.format
-
-local function pad(c, m)
-  local padch = ' '
-  return string.rep(padch, string.len(tostring(m)) - string.len(tostring(c)))
-end
-
-local function get_line()
-  local nbline = fn.line('$')
-  local line = fn.line('.')
-  return fmt('%s%d', pad(line, nbline), line)
-end
-
-local function get_column()
-  local col = fn.col('.')
-  return fmt('%s%d', pad(col, 10), col)
-end
-
-local function get_item()
-  return table.concat({'',get_line(), ':', get_column()})
-end
-
 g['loaded_python_provider'] = 1
 g['python3_host_prog'] = '/usr/bin/python3'
 g['mapleader'] = ","
@@ -41,15 +18,19 @@ g['mapleader'] = ","
 -------------------- PLUGINS -------------------------------
 cmd 'packadd paq-nvim'               -- load the package manager
 local paq = require('paq-nvim').paq  -- a convenient alias
+paq {'savq/paq-nvim', opt = true}    -- paq-nvim manages itself
+paq {'alvan/vim-closetag'}
 paq {'akinsho/nvim-bufferline.lua'}
 paq {'airblade/vim-gitgutter'}
 paq {'airblade/vim-rooter'}
+paq {'b3nj5m1n/kommentary'}
 paq {'dstein64/nvim-scrollview'}
 paq {'elixir-editors/vim-elixir'}
 paq {'farmergreg/vim-lastplace'}
 paq {'joshdick/onedark.vim'}
 paq {'junegunn/fzf'}
 paq {'junegunn/fzf.vim'}
+paq {'junegunn/vim-easy-align'}
 paq {'justinmk/vim-gtfo'}            -- ,gof open file in filemanager
 paq {'kristijanhusak/vim-dadbod-completion'}
 paq {'kyazdani42/nvim-web-devicons'}
@@ -60,32 +41,33 @@ paq {'mattn/emmet-vim'}
 -- paq {'mfussenegger/nvim-dap'}        -- Debug Adapter Protocol
 paq {'neovim/nvim-lspconfig'}
 paq {'norcalli/nvim-colorizer.lua'}
+paq {'norcalli/nvim-terminal.lua'}
 paq {'nvim-lua/completion-nvim'}
 paq {'nvim-treesitter/nvim-treesitter'}
 paq {'ojroques/nvim-bufdel'}
 paq {'ojroques/nvim-hardline'}
 paq {'ojroques/nvim-lspfuzzy'}
 paq {'pbrisbin/vim-mkdir'}           -- :e this/does/not/exist/file.txt then :w
-paq {'savq/paq-nvim', opt = true}    -- paq-nvim manages itself
 paq {'slashmili/alchemist.vim'}
 paq {'steelsojka/completion-buffers'}
 paq {'terryma/vim-smooth-scroll'}
-paq {'tpope/vim-commentary'}
-paq {'tpope/vim-fugitive'}
 paq {'tpope/vim-dadbod'}
+paq {'tpope/vim-endwise'}
+paq {'tpope/vim-fugitive'}
+paq {'Yggdroot/indentLine'}
 
 -------------------- PLUGIN SETUP --------------------------
 -- bufdel
 map('n', '<leader>w', '<cmd>BufDel<CR>')
 require('bufdel').setup {next = 'alternate'}
+-- closetag
+g['closetag_filenames'] = '*.html, *.vue, *.ex, *.eex, *.leex'
 -- colorizer
 require('colorizer').setup {'css'; 'javascript'; html = { mode = 'foreground'; }}
--- dirvish
-g['dirvish_mode'] = [[:sort ,^.*[\/],]]
 -- elixir
 g['alchemist_tag_disable'] = 1
 -- emmet
-vim.api.nvim_exec([[imap <C-e> <C-y>,<CR>]], false)
+map('i', '<C-e>', '<C-y>,', {noremap=false, silent=true})
 -- fzf
 map('n', '<C-p>', '<cmd>Files<CR>')
 map('n', '<leader>g', '<cmd>Commits<CR>')
@@ -93,6 +75,23 @@ map('n', '<C-f>', '<cmd>Rg<CR>')
 map('n', 's', '<cmd>Buffers<CR>')
 g['fzf_action'] = {['ctrl-s'] = 'split', ['ctrl-v'] = 'vsplit'}
 -- hardline
+local fmt = string.format
+local function pad(c, m)
+  local padch = ' '
+  return string.rep(padch, string.len(tostring(m)) - string.len(tostring(c)))
+end
+local function get_line()
+  local nbline = fn.line('$')
+  local line = fn.line('.')
+  return fmt('%s%d', pad(line, nbline), line)
+end
+local function get_column()
+  local col = fn.col('.')
+  return fmt('%s%d', pad(col, 10), col)
+end
+local function get_item()
+  return table.concat({'',get_line(), ':', get_column()})
+end
 require('hardline').setup {
   sections = {
     {class = 'mode', item = require('hardline.parts.mode').get_item},
@@ -107,28 +106,37 @@ require('hardline').setup {
     {class = 'mode', item = get_item},
   },
 }
+-- kommentary
+g['kommentary_create_default_mappings'] = false
+map('n', '<leader>cc', '<Plug>kommentary_line_default', { noremap = false })
+map('n', '<leader>c', '<Plug>kommentary_motion_default', { noremap = false })
+map('v', '<leader>c', '<Plug>kommentary_visual_default', { noremap = false })
+require('kommentary.config').setup()
 -- nvim-bufferline
-require('bufferline').setup{}
+require('bufferline').setup{highlights = {buffer_selected = {gui = ""}}}
+-- nvim-terminal
+require('terminal').setup()
 -- nvim-tree
 map('n', '<C-\\>', '<cmd>NvimTreeToggle<CR>')
-vim.api.nvim_exec([[
-  xnoremap <expr> <Plug>(DBExe) db#op_exec()
-  nnoremap <expr> <Plug>(DBExe) db#op_exec()
-  nnoremap <expr> <Plug>(DBExeLine) db#op_exec() . '_'
-  xmap <leader>p <Plug>(DBExe)
-  nmap <leader>p <Plug>(DBExe)
-  omap <leader>p <Plug>(DBExe)
-  nmap <leader>p <Plug>(DBExeLine)
-  ]], false)
+g['nvim_tree_width'] = 26
+-- vim-easy-align
+map('x', 'ga', '<Plug>(EasyAlign)', {noremap=false})
 -- vim-dadbod
 g['db'] = "postgresql://hvaria:@localhost/mgp_dev"
+map('x', '<Plug>(DBExe)', 'db#op_exec()', {expr=true})
+map('n', '<Plug>(DBExe)', 'db#op_exec()', {expr=true})
+map('n', '<Plug>(DBExeLine)', 'db#op_exec() . \'_\'', {expr=true})
+map('x', '<leader>p', '<Plug>(DBExe)', {noremap=false})
+map('n', '<leader>p', '<Plug>(DBExe)', {noremap=false})
+map('o', '<leader>p', '<Plug>(DBExe)', {noremap=false})
+map('n', '<leader>p', '<Plug>(DBExeLine)', {noremap=false})
 -- vim-sandwich
 cmd 'runtime macros/sandwich/keymap/surround.vim'
 -- vim-smooth-scroll
 map('n', '<C-e>', '<C-u>')
 map('n', '<C-u>', '<C-e>')
-map('n', '<c-e>', ':call smooth_scroll#up(&scroll, 15, 2)<CR>')
-map('n', '<c-d>', ':call smooth_scroll#down(&scroll, 15, 2)<CR>')
+map('n', '<c-e>', ':call smooth_scroll#up(&scroll, 15, 2)<CR>', {silent=true})
+map('n', '<c-d>', ':call smooth_scroll#down(&scroll, 15, 2)<CR>', {silent=true})
 -- vimtex
 g['vimtex_quickfix_mode'] = 0
 g['vimtex_view_method'] = 'evince'
@@ -156,13 +164,13 @@ o.wildmode = 'list:longest'               -- Command-line completion mode
 o.backup = false
 o.writebackup = false
 o.swapfile = false
-o.undodir ='$HOME/.data/undofile'
+o.undodir = '~/.data/undofile'
 o.undofile = true
 o.undolevels = 1000
 o.undoreload = 1000
 -- window-local options
 wo.colorcolumn = tostring(width)          -- Line length marker
-wo.cursorline = false                     -- Highlight cursor line
+wo.cursorline = true                      -- Highlight cursor line
 wo.list = true                            -- Show some invisible characters
 wo.number = true                          -- Show line numbers
 wo.relativenumber = false                 -- Relative line numbers
@@ -188,6 +196,13 @@ map('n', '<F4>', '<cmd>set spell!<CR>')
 map('n', '<leader>t', '<cmd>terminal<CR>')
 map('i', '<C-u>', '<C-g>u<C-u>')
 map('i', '<C-w>', '<C-g>u<C-w>')
+-- move lines up/down
+map('n', '<A-j>', ':m .+1<CR>==')
+map('n', '<A-k>', ':m .-2<CR>==')
+map('i', '<A-j>', '<Esc>:m .+1<CR>==gi')
+map('i', '<A-k>', '<Esc>:m .-2<CR>==gi')
+map('v', '<A-j>', ':m \'>+1<CR>gv=gv')
+map('v', '<A-k>', ':m \'<-2<CR>gv=gv')
 -- Escape
 map('i', 'jk', '<ESC>')
 map('t', 'jk', '<ESC>', {noremap = false})
@@ -217,7 +232,7 @@ map('n', '<leader><Left>', '<cmd>cprev<CR>')
 map('n', '<leader><Right>', '<cmd>cnext<CR>')
 map('n', '<leader><Up>', '<cmd>copen<CR>')
 -- paste
-map('', '<leader>c', '"+y')
+map('', '<leader>y', '"+y')
 -- reselect visual block after indent
 map('v', '<', '<gv')
 map('v', '>', '>gv')
@@ -270,4 +285,5 @@ vim.tbl_map(function(c) cmd(string.format('autocmd %s', c)) end, {
   'FileType elixir,eelixir iab pp \\|>',
   'FileType elixir,eelixir setlocal omnifunc=v:lua.vim.lsp.omnifunc',
   'BufWritePre *.{ex,exs} lua vim.lsp.buf.formatting_sync()',
+  'FileType sql setlocal omnifunc=vim_dadbod_completion#omni',
 })
