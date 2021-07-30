0189a0afa83121e4a4ffb07615745d4b2e2c964e
Hardik Varia <hardikvaria@gmail.com>
2021-07-30 23:58:24 +0000

    Fix git issues again

--- setup.sh
+++ setup.sh
@@ -23,17 +23,10 @@ sudo pacman-mirrors -f 5
 # sudo mhwd -i pci video-hybrid-intel-nvidia-bumblebee
 
 # packages
-<<<<<<< HEAD
 sudo pacman -Syu zsh neovim python-pynvim fzf ripgrep qterminal ttf-fira-code\
   erlang elixir fop wxgtk inotify-tools postgresql
 # size 9 in qterminal for fira mono retina 
 # dconf write /org/gnome/terminal/legacy/profiles:/:<12312>/font "'IBM Plex Mono Bold 9.375'"
-=======
-sudo pacman -Syu zsh neovim python-pynvim fzf ripgrep kitty \
-  yay ttf-fira-code erlang elixir fop wxgtk inotify-tools postgresql
-# size 12 in konsole or 9 for fira code light
-# size 12 in gnome-terminal with Inconsolata SemiBold
->>>>>>> 8df706b5948e8b344bbd80ed309a65d7ec3a72b4
 # optional
 # weechat pdfarranger
 
@@ -46,7 +39,6 @@ git config --global user.name  "Hardik Varia"
 
 # neovim
 mkdir -p ~/.config/nvim
-ln -sf ~/dotfiles/kitty.conf ~/.config/kitty/kitty.conf
 ln -sf ~/dotfiles/init.vim ~/.config/nvim/init.vim
 ln -sf ~/dotfiles/config.yaml ~/.config/efm-langserver/config.yaml
 nvim +PackerSync
