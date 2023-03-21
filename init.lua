f1208fbee80043242537ac74ed14d41bb6f23446
Hardik Varia <hardikvaria@gmail.com>
2023-03-21 11:56:44 +0000

    Tweak neo-tree. Added Trouble and Todo plugins

--- init.lua
+++ init.lua
@@ -304,10 +304,61 @@ require('lazy').setup({
         { "<a-h>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
       },
     },
+    -- better diagnostics list and others
+    {
+      "folke/trouble.nvim",
+      cmd = { "TroubleToggle", "Trouble" },
+      opts = { use_diagnostic_signs = true },
+      keys = {
+        { "<leader>xx", "<cmd>TroubleToggle document_diagnostics<cr>",  desc = "Document Diagnostics (Trouble)" },
+        { "<leader>xX", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostics (Trouble)" },
+        { "<leader>xL", "<cmd>TroubleToggle loclist<cr>",               desc = "Location List (Trouble)" },
+        { "<leader>xQ", "<cmd>TroubleToggle quickfix<cr>",              desc = "Quickfix List (Trouble)" },
+        {
+          "[q",
+          function()
+            if require("trouble").is_open() then
+              require("trouble").previous({ skip_groups = true, jump = true })
+            else
+              vim.cmd.cprev()
+            end
+          end,
+          desc = "Previous trouble/quickfix item",
+        },
+        {
+          "]q",
+          function()
+            if require("trouble").is_open() then
+              require("trouble").next({ skip_groups = true, jump = true })
+            else
+              vim.cmd.cnext()
+            end
+          end,
+          desc = "Next trouble/quickfix item",
+        },
+      },
+    },
+
+    -- todo comments
+    {
+      "folke/todo-comments.nvim",
+      cmd = { "TodoTrouble" },
+      event = { "BufReadPost", "BufNewFile" },
+      config = true,
+      -- stylua: ignore
+      keys = {
+        { "]t",         function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
+        { "[t",         function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
+        { "<leader>xt", "<cmd>TodoTrouble<cr>",                              desc = "Todo (Trouble)" },
+        { "<leader>xT", "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>",      desc = "Todo/Fix/Fixme (Trouble)" },
+      },
+    },
+
     -- Better Escape
     {
       'max397574/better-escape.nvim', config = true
     },
+
     -- auto completion
     { "windwp/nvim-autopairs",    config = true },
     {
@@ -578,11 +629,19 @@ require('lazy').setup({
           follow_current_file = true,
         },
         window = {
-          width = "28",
+          width = "30",
           mappings = {
             ["<space>"] = "none",
           },
         },
+        default_component_configs = {
+          indent = {
+            with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+            expander_collapsed = "",
+            expander_expanded = "",
+            expander_highlight = "NeoTreeExpander",
+          },
+        }
       },
     },
     -- search/replace in multiple files
