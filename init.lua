79e68ec63633c68003870562998ba62d926ce9fd
Hardik Varia <hardikvaria@gmail.com>
2024-02-02 10:15:24 +0000

    Move back to elixirls and elixir-tools

--- init.lua
+++ init.lua
@@ -250,34 +250,34 @@ require("lazy").setup({
       end
 
       local lspconfig = require("lspconfig")
-      local configs = require("lspconfig.configs")
+      -- local configs = require("lspconfig.configs")
 
       local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-      local lexical_config = {
-        filetypes = { "elixir", "eelixir", "heex" },
-        cmd = { "/home/hvaria/lexical/_build/dev/package/lexical/bin/start_lexical.sh" },
-        settings = {},
-      }
-
-      if not configs.lexical then
-        configs.lexical = {
-          default_config = {
-            filetypes = lexical_config.filetypes,
-            cmd = lexical_config.cmd,
-            root_dir = function(fname)
-              return lspconfig.util.root_pattern("mix.exs", ".git")(fname) or vim.loop.os_homedir()
-            end,
-            -- optional settings
-            settings = lexical_config.settings,
-          },
-        }
-      end
-
-      lspconfig.lexical.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
+      -- local lexical_config = {
+      --   filetypes = { "elixir", "eelixir", "heex" },
+      --   cmd = { "/home/hvaria/lexical/_build/dev/package/lexical/bin/start_lexical.sh" },
+      --   settings = {},
+      -- }
+      --
+      -- if not configs.lexical then
+      --   configs.lexical = {
+      --     default_config = {
+      --       filetypes = lexical_config.filetypes,
+      --       cmd = lexical_config.cmd,
+      --       root_dir = function(fname)
+      --         return lspconfig.util.root_pattern("mix.exs", ".git")(fname) or vim.loop.os_homedir()
+      --       end,
+      --       -- optional settings
+      --       settings = lexical_config.settings,
+      --     },
+      --   }
+      -- end
+      --
+      -- lspconfig.lexical.setup({
+      --   on_attach = on_attach,
+      --   capabilities = capabilities,
+      -- })
 
       local elixir = require("elixir")
       local elixirls = require("elixir.elixirls")
@@ -301,10 +301,11 @@ require("lazy").setup({
         },
         credo = { enable = true },
         elixirls = {
-          enable = false,
+          enable = true,
           settings = elixirls.settings({
             dialyzerEnabled = false,
             enableTestLenses = true,
+            suggestSpecs = true,
           }),
           on_attach = on_attach,
           capabilities = capabilities,
