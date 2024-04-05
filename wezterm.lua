736bb0e8617c5513006201a3bcb8cd51d7abd6f2
Hardik Varia <hardikvaria@gmail.com>
2024-04-05 01:11:20 +0000

    Let use wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -34,21 +34,26 @@ table.insert(mykeys, { key = "j", mods = "CTRL|ALT", action = act.AdjustPaneSize
 table.insert(mykeys, { key = "k", mods = "CTRL|ALT", action = act.AdjustPaneSize({ "Up", 1 }) })
 table.insert(mykeys, { key = "l", mods = "CTRL|ALT", action = act.AdjustPaneSize({ "Right", 1 }) })
 
-config.line_height = 1.07
+-- config.line_height = 1.00
 config.font = wezterm.font_with_fallback({
 	{
-		family = "Fira Code",
+		family = "JetBrains Mono",
 		weight = "Medium",
 		harfbuzz_features = {
-			-- "calt=1",
-			-- "clig=1",
-			-- "liga=1",
+			"calt",
+			"clig",
+			"liga",
+			"zero",
+			-- "ss01", -- Classic Construction
+			"ss19", -- == !==
+			"cv02", -- t
+			"cv17", -- f
 		},
 	},
 	{ family = "Symbols Nerd Font Mono", scale = 0.65 },
 })
 config.use_cap_height_to_scale_fallback_fonts = true
-config.font_size = 10.49
+config.font_size = 10.5
 config.anti_alias_custom_block_glyphs = false
 config.warn_about_missing_glyphs = false
 config.default_cursor_style = "SteadyBar"
